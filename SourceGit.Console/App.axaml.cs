b5fd7578ede77fc1980b6edc65c2902bd58ecf9c
Tom Laird-McConnell <thermous@iciclecreek.com>
2025-08-31 16:31:54 -0700

    keep

---
+++ SourceGit.Console/App.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using SourceGit;
+
+namespace SourceGit.Console
+{
+    public partial class App : SourceGit.App
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+      
+    }
+}
